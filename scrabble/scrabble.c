60585ee5e87cbcdf1921260fd1a3ade8e6f20469
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-11-01 22:24:35 +0100

    Create scrabble.c

---
+++ scrabble/scrabble.c
@@ -0,0 +1,71 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins! \n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins! \n");
+    }
+    else if (score1 == score2)
+    {
+        printf("Tie! \n");
+    }
+    else
+    {
+        printf("Please re-try, your input is invalid.");
+    }
+        
+}
+
+const int letters = 26;
+string alphabet = "abcdefghikjlmnopqrstuvwxyz";
+
+
+int compute_score(string word)
+{
+    int n = strlen(word);
+    int score = 0;
+    
+    for (int i = 0; i < n; i++)
+    {
+        //Checks if letter if upper, and if it is, make letter lower case.
+        if (isupper(word[i]) != 0)
+        {
+            word[i] = tolower(word[i]);
+        }
+
+        //Find letter in words position in the alphabet to calculate its score
+        for (int m = 0; m < letters; m++)
+        {     
+            if (word[i] == alphabet[m])
+            {
+                score = score + POINTS[m];
+            }      
+        }        
+    }
+    return score;
+}
+
+
