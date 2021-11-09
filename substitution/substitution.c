809b83a8f33ba46463ce537bbc8eb51bb5f34706
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-11-09 19:32:56 +0100

    Update substitution.c

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -9,7 +9,7 @@ int main(int argc, string argv[])
 {    
     string plaintext = get_string("plaintext: ");
     char ciphertext[strlen(plaintext) + 1];
-    
+
     if (strlen(argv[1]) != 26)
     {
         printf("Key must contain 26 characters. \n");
@@ -22,45 +22,44 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    for(int i=0; i<strlen(argv[1]); i++)
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
         if (isalpha(argv[1][i]) == 0)
         {
             printf("Key must be letters only! \n");
             return 1;
         } 
-        else if (argv[1][i] == argv[1][i+1])
+        else if (argv[1][i] == argv[1][i + 1])
         {
             printf("Key cannot have any repeated characters! \n");
             return 1;           
         }
         else
         {
-            for(int j = 0; j < strlen(argv[1])+1; j++)
-                {        
-                    if (isalpha(plaintext[j]))
+            for (int j = 0; j < strlen(argv[1]) + 1; j++)
+            {        
+                if (isalpha(plaintext[j]))
+                {
+                    if (isupper(plaintext[j]))
                     {
-                        if(isupper(plaintext[j]))
-                        {
-                            //ciphertext[i] = plaintext[i] - 'A'; //FEIL HER
-                            int position = plaintext[j] - 'A';
-                            ciphertext[i] = toupper(argv[1][position]); 
-                        }
-                        else
-                        {
-                            //ciphertext[i] = plaintext[i] - 'a'; //FEIL HER
-                            int position = plaintext[j] - 'a';
-                            ciphertext[j] = tolower(argv[1][position]); 
-                        }    
+                        int position = plaintext[j] - 'A';
+                        ciphertext[i] = toupper(argv[1][position]); 
                     }
                     else
                     {
-                        ciphertext[j] = plaintext[j];
+                        int position = plaintext[j] - 'a';
+                        ciphertext[j] = tolower(argv[1][position]); 
                     }
-                    
                 }
-       }
+                else
+                {
+                    ciphertext[j] = plaintext[j];
+                }
+                    
+            }
+        }
     }
+
     printf("ciphertext: %s\n", ciphertext);  
     return 0;
 }
